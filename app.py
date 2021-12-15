d9f1e26eff07a2d0c13f41b26e4894a545834cdf
MaximeDubost <maxime.dubost.75@gmail.com>
2021-12-15 12:37:47 +0100

    Code cleaning

--- app.py
+++ app.py
@@ -4,25 +4,43 @@ import pandas as pd
 import numpy as np
 import argparse
 from album import Album
+import datetime
 
 # Ajout d'un paramètre facultatif permettant de sélectionner une autre semaine que la semaine courante
 parser = argparse.ArgumentParser()
-parser.add_argument('--year', help='year "20XX" or "Tout" for all time (current year by default)')
-parser.add_argument('--week', help='week number between 1 and 53 (current week by default)')
+parser.add_argument('--year', help='(optional) year between "2001" and the current year (current year by default)')
+parser.add_argument('--week', help='(optional) week number between "1" and "53" (current week by default)')
 args = parser.parse_args()
 
 url = 'https://snepmusique.com/les-tops/le-top-de-la-semaine/top-albums/'
 
-if args.year:
-    print(f'year: {args.year}')
-    url = url.append(f'?annee={args.year}')
-
-if args.week:
-    print(f'week: {args.week}')
-    if '?' in url:
-        url = url.append(f'&semaine={args.week}')
+try:
+    if args.year:
+        if args.year.isdigit() and int(args.year) > 2001 and int(args.year) <= datetime.date.today().year:
+            print(f'year: {args.year}')
+            url = url.append(f'?annee={args.year}')
+        else:
+            print('Invalid year: must be between "2001" and the current year')
     else:
-        url = url.append(f'?semaine={args.week}')
+        print(f'year: {datetime.date.today().year}')
+
+    if args.week:
+        if args.week.isdigit() and int(args.week) >= 1 and int(args.year) <= 53:
+            print(f'week: {args.week}')
+            if args.year in url :
+                url = url.append(f'&semaine={args.week}')
+            else:
+                url = url.append(f'?semaine={args.week}')
+        else:
+            print('Invalid week: must be between "1" and "53"')
+    else:
+        print(f'week: {datetime.date.today().strftime("%V")}')
+
+except Exception:
+    print(f'An error has occured')
+    print(f'year: {datetime.date.today().year}')
+    print(f'week: {datetime.date.today().strftime("%V")}')
+    url = 'https://snepmusique.com/les-tops/le-top-de-la-semaine/top-albums/'
 
 request_text = requests.get(url)
 
@@ -40,15 +58,23 @@ df['best_rank'] = df['best_rank'].astype('int')
 
 for element in soup.find_all('div', class_='item'):
     
+    rank = element.find('div', class_='rang')
+    title = element.find('div', class_='titre')
+    artist = element.find('div', class_='artiste')
+    editor = element.find('div', class_='editeur')
+    last_week_rank = element.find('div', class_='rang_precedent')
+    week_in = element.find('div', class_='week_in')
+    best_rank = element.find('div', class_='best_pos')
+
     album = Album(
-        element.find('div', class_='rang').get_text(),
+        rank.get_text(),
         'Up' if(element.find('div', class_='rang_up icon-bigarrowup') != None) else ('Down' if(element.find('div', class_='rang_down icon-bigarrowdown') != None) else 'Neutral'),
-        element.find('div', class_='titre').get_text(),
-        element.find('div', class_='artiste').get_text(),
-        element.find('div', class_='editeur').get_text(),
-        -1 if element.find('div', class_='rang_precedent') == None else int(element.find('div', class_='rang_precedent').find('strong').get_text()[0:-2 if 'er' in element.find('div', class_='rang_precedent').find('strong').get_text() else -1]),
-        0 if element.find('div', class_='week_in').find('strong').get_text() == 'Nouvelle entrée' else int(element.find('div', class_='week_in').find('strong').get_text()[0:-2 if 'er' in element.find('div', class_='week_in').find('strong').get_text() else -1]),
-        int(element.find('div', class_='best_pos').find('strong').get_text()[0:-2 if 'er' in element.find('div', class_='best_pos').find('strong').get_text() else -1]),
+        title.get_text(),
+        artist.get_text(),
+        editor.get_text(),
+        -1 if last_week_rank == None else int(last_week_rank.find('strong').get_text()[0:-2 if 'er' in last_week_rank.find('strong').get_text() else -1]),
+        0 if week_in.find('strong').get_text() == 'Nouvelle entrée' else int(week_in.find('strong').get_text()[0:-2 if 'er' in week_in.find('strong').get_text() else -1]),
+        int(best_rank.find('strong').get_text()[0:-2 if 'er' in best_rank.find('strong').get_text() else -1]),
     )
     
     df = df.append({
